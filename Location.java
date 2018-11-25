6264c45e256a46a278892b008f7be91d82d51a0b
JaniceG2018 <791911642@qq.com>
2018-11-25 16:10:05 -0500

    GPS and MainActivity

--- Location.java
+++ Location.java
@@ -1,21 +1,22 @@
-package simplejson;
+package com.ja.getdevicelocation;
+
 public class Location {
-private double lat;
-private double lon;
-Location(double lat,double lon){
-	this.lat=lat;
-	this.lon=lon;
-}
-public double getLat() {
-	return lat;
-}
-public void setLat(double lat) {
-	this.lat = lat;
-}
-public double getLon() {
-	return lon;
-}
-public void setLon(double lon) {
-	this.lon = lon;
-}
+    private double lat;
+    private double lon;
+    Location(double lat,double lon){
+        this.lat=lat;
+        this.lon=lon;
+    }
+    public double getLat() {
+        return lat;
+    }
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+    public double getLon() {
+        return lon;
+    }
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
 }
--- WeatherAPI.java
+++ WeatherAPI.java
@@ -1,4 +1,6 @@
-package simplejson;
+package com.ja.getdevicelocation;
+import android.os.AsyncTask;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,103 +20,51 @@ import org.json.JSONObject;
  *
  */
 public class WeatherAPI {
-	
-	/**
-	 * Parse the JSON response String
-	 * @param jsonResponse
-	 * @return ArrayList of RecipePuppyRecipe objects
-	 * @throws JSONException
-	 */
-	public ArrayList<WeatherInfo> parseWeatherJSON(String jsonResponse) throws JSONException {
-		//create a JSON object with the String response
-		JSONObject jObj = new JSONObject(jsonResponse);
-		//Look at the raw String response
-		//Look for the results key
-		//After the colon there is a square bracket indicating a JSONArray
-		JSONArray jArray1 = jObj.getJSONArray("weather");
-		
-		
-		
-		ArrayList<WeatherInfo> weatherArray = new ArrayList<WeatherInfo>();
-		
-		
-			String mainWeather=jArray1.getJSONObject(0).getString("main");
-			double tempMin=jObj.getJSONObject("main").getDouble("temp_min");
-			double tempMax=jObj.getJSONObject("main").getDouble("temp_max");
-		    double pressure=jObj.getJSONObject("main").getDouble("pressure");
-		    int humid=jObj.getJSONObject("main").getInt("humidity");
-			String city=jObj.getString("name");
-			WeatherInfo rpr = new WeatherInfo(city,tempMin,tempMax,mainWeather,pressure, humid);
-			weatherArray.add(rpr);
-		
-		return weatherArray;
-	}
-	
-	/**
-	 * Makes the API call and returns the JSON result as a String
-	 * @param url
-	 * @return
-	 * @throws IOException
-	 */
-	public String makeAPICall(String url) throws IOException {
-		URL recipeWeather;
-		URLConnection yc;
-		BufferedReader in;
-		
-		recipeWeather = new URL(url);
-		yc = recipeWeather.openConnection();
-		in = new BufferedReader(new InputStreamReader(
-                 yc.getInputStream()));
-		String inputLine;
-		
-		//Why StringBuffer? - StringBuffer is mutable so we can append to it
-		StringBuffer response = new StringBuffer();
-		//BufferedReader does not have a "hasNext" type method so this is how to check for 
-		//more input
-		//if it has more input append to the StringBuffer
-		while ((inputLine = in.readLine()) != null) {
-		     response.append(inputLine);
-		}
-		in.close();
-		     
-		return response.toString();
-	}
-	
-	public static void main(String[] args) {
-		Location place=new Location(11,33);
-		//create the API URL
-		String endPoint="https://api.openweathermap.org";
-		String url2="/data/2.5/weather?lat=";
-		double latNum=place.getLat();
-		String lon="&lon=";
-		double lonNum=place.getLon();
-		String key="&appid=93878e3b8fc1c0224d9e17f353cf474f";
-		String weatherUrl =endPoint+url2+latNum+lon+lonNum+key;
-		
-		WeatherAPI recipePuppy = new WeatherAPI();
-		ArrayList<WeatherInfo> weatherInfos = new ArrayList<>();
-		
-		try {
-			//make the API call and get a String response
-			String jsonResponse = recipePuppy.makeAPICall(weatherUrl);
-			//parse the response and get an ArrayList of RecipePuppyRecipe objects
-			weatherInfos = recipePuppy.parseWeatherJSON(jsonResponse);
-			//view the results in a proper Java object
-			for(WeatherInfo wInfo: weatherInfos) {
-				System.out.println("city name = "+wInfo.getCity());
-				System.out.println("main weather = "+wInfo.getMainWeather());
-				System.out.println("temp max = "+wInfo.getTemMax());
-				System.out.println("temp min = "+wInfo.getTemMin());
-				System.out.println("humidity = "+wInfo.getHumid());
-				System.out.println("pressure = "+wInfo.getPressure());
-				
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	} 
+
+    private Exception exception;
+    /**
+     * Parse the JSON response String
+     * @param jsonResponse
+     * @return ArrayList of RecipePuppyRecipe objects
+     * @throws JSONException
+     */
+    public WeatherInfo parseWeatherJSON(String jsonResponse) throws JSONException {
+        //create a JSON object with the String response
+        JSONObject jObj = new JSONObject(jsonResponse);
+        //Look at the raw String response
+        //Look for the results key
+        //After the colon there is a square bracket indicating a JSONArray
+        JSONArray jArray1 = jObj.getJSONArray("weather");
+        String mainWeather=jArray1.getJSONObject(0).getString("main");
+        double tempMin=jObj.getJSONObject("main").getDouble("temp_min");
+        double tempMax=jObj.getJSONObject("main").getDouble("temp_max");
+        double pressure=jObj.getJSONObject("main").getDouble("pressure");
+        int humid=jObj.getJSONObject("main").getInt("humidity");
+        String city=jObj.getString("name");
+        WeatherInfo wgeo = new WeatherInfo(city,tempMin,tempMax,mainWeather,pressure, humid);
+
+        return wgeo;
+    }
+
+    public String createURL(double latNum, double lonNum){
+        String endPoint = "https://api.openweathermap.org";
+        String url2 = "/data/2.5/weather?lat=";
+        String lon = "&lon=";
+        String key = "&appid=93878e3b8fc1c0224d9e17f353cf474f";
+        String weatherUrl = endPoint + url2 + latNum + lon + lonNum + key;
+        return  weatherUrl;
+    }
+    public WeatherInfo getWgeo(String response) {
+
+        WeatherAPI weatherAPI = new WeatherAPI();
+        WeatherInfo wgeo=new WeatherInfo("",0.0,0.0,"",0.0,0);
+        try {
+            wgeo = weatherAPI.parseWeatherJSON(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return wgeo;
+    }
+
+
 }
