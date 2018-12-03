76769e2a18ff6c17e43f180c99bd1355ab4801c8
ysun822 <42682974+ysun822@users.noreply.github.com>
2018-12-03 15:52:38 -0500

    yilin latest version

--- MainActivity.java
+++ MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.ImageView;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -28,35 +28,45 @@ import java.util.List;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView city;
     TextView mainWeather;
     TextView pressure;
-    TextView temp;
+    TextView temperature;
+    TextView humid;
+    TextView tempMax;
+    TextView tempMin;
+    ImageView weatherImage;
     private FusedLocationProviderClient client;
 
-    public static String textResult="";
-    public static String cityURL="";
+    public static String textResult = "";
+    public static String textResult2="";
+    public static String cityURL = "";
+    public static String forecastCityURL="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestPermission();
 
-        final WeatherAPI weatherAPI=new WeatherAPI();
-        final ForecastAPI forecastAPI=new ForecastAPI();
+        final WeatherAPI weatherAPI = new WeatherAPI();
+        final ForecastAPI forecastAPI = new ForecastAPI();
         client = LocationServices.getFusedLocationProviderClient(this);
-        Button buttonSearch=findViewById(R.id.citySearch);
-        buttonSearch.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Log.i("MainActivity","Search!");
-                Intent intent=new Intent(MainActivity.this, SearchInfo.class);
+        Button buttonSearch = findViewById(R.id.citySearch);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.i("MainActivity", "Search!");
+                Intent intent = new Intent(MainActivity.this, SearchInfo.class);
                 EditText editText = (EditText) findViewById(R.id.editText);
                 String cityName = editText.getText().toString();
-                Log.i("MainActivitycityName",cityName);
-                CityAPI cityAPI=new CityAPI();
-                cityURL=cityAPI.createURL(cityName);
-                Log.i("cityURL",cityURL);
-                CityTask cityTask=new CityTask();
+                Log.i("MainActivitycityName", cityName);
+                CityAPI cityAPI = new CityAPI();
+                ForecastCityAPI forecastCityAPI=new ForecastCityAPI();
+                cityURL = cityAPI.createURL(cityName);
+                forecastCityURL=forecastCityAPI.createURL(cityName);
+                Log.i("cityURL", cityURL);
+                CityTask cityTask = new CityTask();
                 cityTask.execute();
 
             }
@@ -65,27 +75,31 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("MainActivity","Clicked!");
+                Log.i("MainActivity", "Clicked!");
 
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     //System.out.print("permission failed!");
                     return;
                 }
                 client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
-                        if (location!=null){
-                            mainWeather=findViewById(R.id.weather);
-                            city=findViewById(R.id.location);
-                            pressure=findViewById(R.id.pressure);
-                            temp=findViewById(R.id.temp);
+                        if (location != null) {
+                            mainWeather = findViewById(R.id.weather);
+                            city = findViewById(R.id.location);
+                            pressure = findViewById(R.id.pressure);
+                            temperature = findViewById(R.id.temperature);
+                            tempMax = findViewById(R.id.tempMax);
+                            tempMin = findViewById(R.id.tempMin);
+                            humid = findViewById(R.id.humid);
+                            weatherImage = findViewById(R.id.WeatherImage);
                             //textView.setText(String.valueOf(location.getLatitude())+" +  "+String.valueOf(location.getLongitude()));
 
-                            String forecastURL=forecastAPI.createURL(location.getLatitude(),location.getLongitude());
+                            String forecastURL = forecastAPI.createURL(location.getLatitude(), location.getLongitude());
 
-                            String locationURL=weatherAPI.createURL(location.getLatitude(),location.getLongitude());
+                            String locationURL = weatherAPI.createURL(location.getLatitude(), location.getLongitude());
                             String[] response;
-                            GeoTask geoTask=new GeoTask();
+                            GeoTask geoTask = new GeoTask();
                             geoTask.execute(forecastURL, locationURL);
 
                             //System.out.print(location.getLatitude());
@@ -96,30 +110,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},1);
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 
-    private class GeoTask extends AsyncTask<String, Void, String[]>{
+    private class GeoTask extends AsyncTask<String, Void, String[]> {
 
         @Override
         protected String[] doInBackground(String... urls) {
-            ForecastAPI forecastAPI=new ForecastAPI();
-            WeatherAPI weatherAPI=new WeatherAPI();
+            ForecastAPI forecastAPI = new ForecastAPI();
+            WeatherAPI weatherAPI = new WeatherAPI();
             URL locationURL;
             URL forecastURL;
-            String[] response=new String[2];
-            try{
-                locationURL=new URL(urls[0]);
-                forecastURL=new URL(urls[1]);
-                String locationResponse=weatherAPI.makeAPICall(locationURL);
-                response[0]=locationResponse;
-                String forecastResponse=forecastAPI.makeAPICall(forecastURL);
-                response[1]=forecastResponse;
-            }catch (MalformedURLException e){
+            String[] response = new String[2];
+            try {
+                locationURL = new URL(urls[0]);
+                forecastURL = new URL(urls[1]);
+                String locationResponse = weatherAPI.makeAPICall(locationURL);
+                response[0] = locationResponse;
+                String forecastResponse = forecastAPI.makeAPICall(forecastURL);
+                response[1] = forecastResponse;
+            } catch (MalformedURLException e) {
 
                 e.printStackTrace();
-            }catch (IOException e){
+            } catch (IOException e) {
 
                 e.printStackTrace();
             }
@@ -128,17 +142,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         @Override
-        protected void onPostExecute( String[] response) {
-            WeatherAPI weatherAPI=new WeatherAPI();
-            ForecastAPI forecastAPI=new ForecastAPI();
-            List<DailyWeather> res=forecastAPI.getGeoForecast(response[1]);
-            Intent intent=new Intent(MainActivity.this, WeatherDisplay.class);
+        protected void onPostExecute(String[] response) {
+            WeatherAPI weatherAPI = new WeatherAPI();
+            ForecastAPI forecastAPI = new ForecastAPI();
+            List<DailyWeather> res = forecastAPI.getGeoForecast(response[1]);
+            Intent intent = new Intent(MainActivity.this, WeatherDisplay.class);
+            intent.putExtra("mainWeather2", res.get(1).getDiscription());
+            intent.putExtra("temp2", String.valueOf(res.get(1).getTemMax()));
             intent.putExtra("mainWeather",weatherAPI.getWgeo(response[0]).getMainWeather());
             intent.putExtra("city",weatherAPI.getWgeo(response[0]).getCity());
-            intent.putExtra("pressure",String.valueOf(weatherAPI.getWgeo(response[0]).getPressure()));
-            intent.putExtra("temp",String.valueOf(weatherAPI.getWgeo(response[0]).getTemMax()));
+            intent.putExtra("pressure","Pressure \n"+String.valueOf(weatherAPI.getWgeo(response[0]).getPressure()));
+            intent.putExtra("temperature",String.valueOf(weatherAPI.getWgeo(response[0]).getTemMax()/2+weatherAPI.getWgeo(response[0]).getTemMin()/2)+" °C");
+            intent.putExtra("humid","Humid \n"+String.valueOf(weatherAPI.getWgeo(response[0]).getHumid()));
+            intent.putExtra("tempMax","Max\n"+String.valueOf(weatherAPI.getWgeo(response[0]).getTemMax())+" °C");
+            intent.putExtra("tempMin","Min\n"+String.valueOf(weatherAPI.getWgeo(response[0]).getTemMin())+" °C");
             //intent.putExtra("fore_temp",res.get())
 
             startActivity(intent);
@@ -147,28 +165,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class CityTask extends AsyncTask<Void, Void, Void>{
+    private class CityTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
+
+
             URL textURL;
-            try{
-                textURL=new URL(cityURL);
-                BufferedReader in=new BufferedReader(new InputStreamReader(
+            URL text2URL;
+            try {
+                textURL = new URL(cityURL);
+                text2URL=new URL(forecastCityURL);
+                BufferedReader in = new BufferedReader(new InputStreamReader(
                         textURL.openStream()));
+                BufferedReader in2 = new BufferedReader(new InputStreamReader(
+                        text2URL.openStream()));
                 String inputLine;
+                String inputLine2;
                 StringBuffer response = new StringBuffer();
+                StringBuffer response2=new StringBuffer();
 
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
-                textResult=response.toString();
+                while ((inputLine2 = in2.readLine()) != null) {
+                    response2.append(inputLine2);
+                }
+                textResult = response.toString();
+                textResult2=response2.toString();
                 in.close();
-            }catch (MalformedURLException e){
-                textResult=e.toString();
+                in2.close();
+            } catch (MalformedURLException e) {
+                textResult = e.toString();
+                textResult2=e.toString();
                 e.printStackTrace();
-            }catch (IOException e){
-                textResult=e.toString();
+            } catch (IOException e) {
+                textResult = e.toString();
+                textResult2 = e.toString();
                 e.printStackTrace();
             }
 
@@ -176,24 +209,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         @Override
         protected void onPostExecute(Void aVoid) {
-            CityAPI cityAPI=new CityAPI();
-            Intent intent=new Intent(MainActivity.this, SearchInfo.class);
-            intent.putExtra("city_mainWeather",cityAPI.getCityWeather(textResult).getMainWeather());
-            Log.i("MainActivityWeather = ",cityAPI.getCityWeather(textResult).getMainWeather());
-            intent.putExtra("city_city",cityAPI.getCityWeather(textResult).getCity());
-            Log.i("MainActivitycity = ",cityAPI.getCityWeather(textResult).getCity());
-            intent.putExtra("city_pressure",String.valueOf(cityAPI.getCityWeather(textResult).getPressure()));
-            Log.i("MainActivitypressure = ",String.valueOf(cityAPI.getCityWeather(textResult).getPressure()));
-            intent.putExtra("city_temp",String.valueOf(cityAPI.getCityWeather(textResult).getTemMax()));
-            Log.i("MainActivitytempMax = ",String.valueOf(cityAPI.getCityWeather(textResult).getTemMax()));
+            CityAPI cityAPI = new CityAPI();
+            ForecastCityAPI forecastCityAPI=new ForecastCityAPI();
+            Intent intent = new Intent(MainActivity.this, SearchInfo.class);
+
+            intent.putExtra("city_mainWeather", cityAPI.getCityWeather(textResult).getMainWeather());
+            Log.i("MainActivityWeather = ", cityAPI.getCityWeather(textResult).getMainWeather());
+            intent.putExtra("city_city", cityAPI.getCityWeather(textResult).getCity());
+            Log.i("MainActivitycity = ", cityAPI.getCityWeather(textResult).getCity());
+            intent.putExtra("city_pressure", String.valueOf(cityAPI.getCityWeather(textResult).getPressure()));
+            Log.i("MainActivitypressure = ", String.valueOf(cityAPI.getCityWeather(textResult).getPressure()));
+            intent.putExtra("city_temp", String.valueOf(cityAPI.getCityWeather(textResult).getTemMax()));
+            Log.i("MainActivitytempMax = ", String.valueOf(cityAPI.getCityWeather(textResult).getTemMax()));
             startActivity(intent);
 
+            intent.putExtra("city_mainWeather2",forecastCityAPI.getCityForecast(textResult2).get(1).getDiscription());
+            Log.i("MainActivityWeather2 = ", forecastCityAPI.getCityForecast(textResult2).get(1).getDiscription());
+
+
+           // intent.putExtra("city_temp2", String.valueOf(forecastCityAPI.getCityForecast(textResult2).get(1).getTemMax()));
+            //Log.i("MainActivitytempMax2 = ", String.valueOf(cityAPI.getCityWeather(textResult).getTemMax()));
             super.onPostExecute(aVoid);
         }
     }
 
 
+    public void onClick(View v) {
+
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+    }
 }
--- WeatherDisplay.java
+++ WeatherDisplay.java
@@ -3,11 +3,16 @@ package com.ja.getdevicelocation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class WeatherDisplay extends AppCompatActivity {
-    private TextView real_weather, real_pressure, location,temp;
+    private TextView real_weather, real_pressure, location,real_temperature,real_humid,real_tempMax,real_tempMin,real_weather1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +22,129 @@ public class WeatherDisplay extends AppCompatActivity {
         String city=intent.getStringExtra("city");
         String pressure=intent.getStringExtra("pressure");
         String mainWeather=intent.getStringExtra("mainWeather");
+       // String mainWeather1=intent.getStringExtra("mainWeather1");
+
+        ImageView view=findViewById(R.id.WeatherImage);
+
+        if(mainWeather.contains("Rain")) {
+            view.setImageResource(R.drawable.rainy);
+        }else if (mainWeather.contains("rain")) {
+            view.setImageResource(R.drawable.rainy);
+        }else if (mainWeather.contains("snow")) {
+            view.setImageResource(R.drawable.snow);
+        }else if (mainWeather.contains("Snow")) {
+            view.setImageResource(R.drawable.snow);
+        } else if (mainWeather.contains("Cloud")) {
+            view.setImageResource(R.drawable.cloudy);
+        } else if (mainWeather.contains("cloud")) {
+            view.setImageResource(R.drawable.cloudy);
+        } else if (mainWeather.contains("Clear")) {
+            view.setImageResource(R.drawable.sunny);
+        } else if (mainWeather.contains("clear")) {
+            view.setImageResource(R.drawable.sunny);
+        } else if (mainWeather.contains("sun")) {
+            view.setImageResource(R.drawable.sunny);
+        }else if (mainWeather.contains("fog")) {
+            view.setImageResource(R.drawable.fog);
+        }else if (mainWeather.contains("haze")) {
+            view.setImageResource(R.drawable.fog);
+        }else if (mainWeather.contains("Fog")) {
+            view.setImageResource(R.drawable.fog);
+        }else if (mainWeather.contains("Haze")) {
+            view.setImageResource(R.drawable.fog);
+        }else{
+            view.setImageResource(R.drawable.sunny);
+        }
+
+       /* ImageView view1=findViewById(R.id.tommImage);
+
+        if(mainWeather1.contains("Rain")) {
+            view1.setImageResource(R.drawable.rainy);
+        }else if (mainWeather1.contains("rain")) {
+            view1.setImageResource(R.drawable.rainy);
+        }else if (mainWeather1.contains("snow")) {
+            view1.setImageResource(R.drawable.snow);
+        }else if (mainWeather1.contains("Snow")) {
+            view1.setImageResource(R.drawable.snow);
+        } else if (mainWeather1.contains("Cloud")) {
+            view1.setImageResource(R.drawable.cloudy);
+        } else if (mainWeather1.contains("cloud")) {
+            view1.setImageResource(R.drawable.cloudy);
+        } else if (mainWeather1.contains("Clear")) {
+            view1.setImageResource(R.drawable.sunny);
+        } else if (mainWeather1.contains("clear")) {
+            view1.setImageResource(R.drawable.sunny);
+        } else if (mainWeather1.contains("sun")) {
+            view1.setImageResource(R.drawable.sunny);
+        }else if (mainWeather1.contains("fog")) {
+            view1.setImageResource(R.drawable.fog);
+        }else if (mainWeather1.contains("haze")) {
+            view1.setImageResource(R.drawable.fog);
+        }else if (mainWeather1.contains("Fog")) {
+            view1.setImageResource(R.drawable.fog);
+        }else if (mainWeather1.contains("Haze")) {
+            view1.setImageResource(R.drawable.fog);
+        }else{
+            view1.setImageResource(R.drawable.sunny);
+        }
+        */
+        Date now=new Date();
+
+        Calendar calendar=new GregorianCalendar();
+        calendar.add(calendar.DATE,1);
+        Date tomorrow1=calendar.getTime();
+        String nowAsString=new SimpleDateFormat("yyyy-MM-dd" ).format(now);
+        TextView day=findViewById(R.id.Day);
+        day.setText(nowAsString);
+        String tomAsString=new SimpleDateFormat("MM-dd").format(tomorrow1);
+        TextView tomorrow=findViewById(R.id.tomorrow);
+        tomorrow.setText(tomAsString);
+
+        calendar.add(calendar.DATE,1);
+        Date tom2=calendar.getTime();
+        String thirdAsString=new SimpleDateFormat("MM-dd").format(tom2);
+        TextView third=findViewById(R.id.third);
+        third.setText(thirdAsString);
+
+        calendar.add(calendar.DATE,1);
+        Date tom3=calendar.getTime();
+        String fourthAsString=new SimpleDateFormat("MM-dd").format(tom3);
+        TextView fourth=findViewById(R.id.fourth);
+        fourth.setText(fourthAsString);
+
+        calendar.add(calendar.DATE,1);
+        Date tom4=calendar.getTime();
+        String fifthAsString=new SimpleDateFormat("MM-dd").format(tom4);
+        TextView fifth=findViewById(R.id.fifth);
+        fifth.setText(fifthAsString);
+
+        calendar.add(calendar.DATE,1);
+        Date tom5=calendar.getTime();
+        String sixthAsString=new SimpleDateFormat("MM-dd").format(tom5);
+        TextView sixth=findViewById(R.id.sixth);
+        sixth.setText(sixthAsString);
+
+        String temperature=intent.getStringExtra("temperature");
+        String tempMax=intent.getStringExtra("tempMax");
+        String tempMin=intent.getStringExtra("tempMin");
+        String humid=intent.getStringExtra("humid");
         location=findViewById(R.id.location);
         real_weather=findViewById(R.id.weather);
         real_pressure=findViewById(R.id.pressure);
-        temp=findViewById(R.id.temp);
+        real_temperature=findViewById(R.id.temperature);
+        real_tempMax=findViewById(R.id.tempMax);
+        real_tempMin=findViewById(R.id.tempMin);
+        real_humid=findViewById(R.id.humid);
+        //real_weather1=findViewById(R.id.tomInfo);
         location.setText(city);
         real_weather.setText(mainWeather);
         real_pressure.setText(pressure);
-        String tempMax=intent.getStringExtra("temp");
-        temp.setText(tempMax);
+        real_temperature.setText(temperature);
+        real_humid.setText(humid);
+        real_tempMax.setText(tempMax);
+        real_tempMin.setText(tempMin);
+
+        //real_weather1.setText(mainWeather1);
 
         location.setOnTouchListener(new OnSwipeTouchListener(WeatherDisplay.this) {
             public void onSwipeTop() {
@@ -43,6 +162,7 @@ public class WeatherDisplay extends AppCompatActivity {
             }
 
         });
+
 
     }
 }
